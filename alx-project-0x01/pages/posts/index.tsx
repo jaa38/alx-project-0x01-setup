4618ee63ef1a82b095f4e450e59a9fd832e9d664
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-26 20:04:04 +0100

    task 1 done

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage: React.FC = () => {
-  return <div>Posts Page</div>;
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+      </main>
+    </div>
+  );
 };
 
 export default PostsPage;
+
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
-  return <div>Users Page</div>;
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Users Page</h1>
+      </main>
+    </div>
+  );
 };
 
 export default UsersPage;
+
