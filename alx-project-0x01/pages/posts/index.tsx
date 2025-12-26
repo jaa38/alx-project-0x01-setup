47f781d9cad4178802db0c65fe6f07f81b68152f
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-26 19:18:56 +0100

    Implement the User Mdoal

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,31 +1,45 @@
-import PostCard from '@/components/common/PostCard';
 import Header from '@/components/layout/Header';
-import { PostProps } from '@/interfaces';
+import UserCard from '@/components/common/UserCard';
+import UserModal from '@/components/common/UserModal';
+import { UserProps, UserData } from '@/interfaces';
+import { useState } from 'react';
 
-const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
-  console.log(posts);
+const Posts: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [user, setUser] = useState<UserData | null>(null);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUser({ ...newUser, id: posts.length + 1 });
+  };
+
   return (
     <div className='flex flex-col h-screen'>
       <Header />
+
       <main className='p-4'>
-        <div className='flex justify-between'>
-          <h1 className='text-2xl font-semibold'>Post Content</h1>
-          <button className='bg-blue-700 px-4 py-2 rounded-full text-white'>
-            Add Post
+        <div className='flex justify-between mb-4'>
+          <h1 className='text-2xl font-semibold'>Users</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className='bg-blue-700 px-4 py-2 rounded-full text white'
+          >
+            Add User
           </button>
         </div>
+
+        <div className='grid grid-cols-3 gap-2'>
+          {posts?.map((user, key) => (
+            <UserCard key={key} {...user} />
+          ))}
+        </div>
       </main>
-      <div className='className="grid grid-cols-3 gap-2"'>
-        {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
-          <PostCard
-            title={title}
-            userId={userId}
-            body={body}
-            id={id}
-            key={key}
-          />
-        ))}
-      </div>
+
+      {isModalOpen && (
+        <UserModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddUser}
+        />
+      )}
     </div>
   );
 };
