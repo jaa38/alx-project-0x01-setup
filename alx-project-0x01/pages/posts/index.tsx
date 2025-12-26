234d0032d4aeb821aa34b035542e58f977efb92a
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-26 11:08:57 +0100

    Set up navigation between pages

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/layout/Header";
+
+const Posts: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">Posts Page</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Posts;
+
+
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/layout/Header";
+
+const Users: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">Users Page</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Users;
+
