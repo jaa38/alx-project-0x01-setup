27cf42591598683673dd3260fa5d52a3d83ce415
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-28 12:05:38 +0100

    Implement Users Card Components 2

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -8,9 +8,21 @@ const Users: React.FC<UserProps[]> = ({ posts }) => {
       <Header />
       <main className='p-4'>
         <h1 className='text-2xl font-semibold mb-4'>Users</h1>
+
         <div className='grid grid-cols-3 gap-4'>
-          {posts?.map((user: UserProps, key: number) => (
-            <UserCard {...user} key={key} />
+          {posts.map((user: UserProps, key: number) => (
+            // <UserCard {...user} key={key} />
+            <UserCard
+              key={key}
+              id={user.id}
+              name={user.name}
+              username={user.username}
+              email={user.email}
+              phone={user.phone}
+              website={user.website}
+              address={user.address}
+              company={user.company}
+            />
           ))}
         </div>
       </main>
